4155d3b98fb9723f63ff537a5fd8982c2d491fea
Artem <selfex9496@gmail.com>
2023-07-24 18:50:51 +0700

    added validation urls and error processing

--- src/app.js
+++ src/app.js
@@ -1,20 +1,49 @@
 import * as yup from 'yup';
+import _ from 'lodash';
 import watch from './view/view.js';
-
-const schema = yup.object().shape({
-  link: yup.string().url(),
-});
+import isValidURL from './utils/isValidURL.js';
 
 export default () => {
+  const schema = yup.object().shape({
+    url: yup.string().url(),
+  });
+
   const state = {
     form: {
-      link: '',
+      valid: false,
     },
+    feeds: {
+      urls: [],
+      valid: false,
+    },
+    errors: [],
   };
 
   const elements = {
-    form: document.querySelector('#rss-form'),
+    form: {
+      formEl: document.querySelector('#rss-form'),
+      input: document.querySelector('#url-input'),
+    },
   };
-console.log(state)
+
   const watchedState = watch(elements, state);
+
+  elements.form.formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    const validateData = schema.validate(data, { abortEarly: false });
+    return validateData.then((valided) => {
+      if (watchedState.feeds.urls.includes(valided.url)) {
+        throw new Error('RSS уже существует');
+      }
+      watchedState.feeds.urls.push(valided.url);
+      watchedState.form.valid = true;
+      watchedState.errors = [];
+    })
+      .catch((error) => {
+        watchedState.errors.push(error.message);
+      });
+  });
 };
--- src/view/view.js
+++ src/view/view.js
@@ -1,8 +1,28 @@
 import onChange from 'on-change';
 
-export default (elements, state) => {
-  const watchedState = onChange(state, () => {
+const renderError = (elements, state) => {
+  const { input } = elements.form;
+  const { errors } = state;
+  if (errors.length === 0) {
+    input.classList.remove('is-invalid');
+  } else {
+    input.classList.add('is-invalid');
+  }
 
+  elements.form.formEl.reset();
+  input.focus();
+};
+
+export default (elements, state) => {
+  const watchedState = onChange(state, (path, value) => {
+    switch (path) {
+      case 'errors': {
+        renderError(elements, state);
+        break;
+      }
+      default:
+        break;
+    }
   });
 
   return watchedState;
