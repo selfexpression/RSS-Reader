0e13a0ef87b8626ab35c3320e933e6031d1b2d84
Artem <selfex9496@gmail.com>
2023-08-08 11:22:54 +0700

    small fix for modal window working

--- src/utils/createPost.js
+++ src/utils/createPost.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 export default (posts, feedID) => posts
   .map((post) => {
-    post.id = _.uniqueId;
+    post.id = _.uniqueId();
     post.feedID = feedID;
     return post;
   });
